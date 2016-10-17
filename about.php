3c6db7d211cbcb95b4c8dad24615140089331bfe
Jake Ruesch <jsruesch@gmail.com>
2016-10-17 08:26:19 -0500

    Clean up mobile nav

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php include('header.php') ?>
+
+<!--
+  BEGIN: Main Content
+-->
+  <div class="container-fluid mc">
+    <div class="container">
+
+      
+
+    </div>
+  </div>
+<!-- END: Main Content -->
+
+<?php include('footer.php') ?>
--- header.php
+++ header.php
@@ -2,7 +2,12 @@
 <html lang="en">
 <head>
     <title>HEVGA</title>
-    <meta name="description" content="">
+    <meta name="description" content="Our mission is to create a platform for higher education leaders which
+    will underscore the cultural, scientific, and economic importance of video game
+    programs in colleges and universities. The key is to create a robust network of
+    resources--including unified advocacy, policymaker engagement, media coverage,
+    and external funding--in order to incubate and harness the impact of this community
+    in a 21st century learning environment.">
     <!-- Main Stylesheet -->
     <link rel="stylesheet" type="text/css" href="stylesheets/style.min.css"/>
     <!-- jQuery -->
@@ -12,6 +17,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Asap:400,700" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 </head>
 <body id="">
 
@@ -37,7 +44,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">About</a></li>
+            <li><a href="about.php">About</a></li>
             <li><a href="#">Reports</a></li>
             <li><a href="news.php">News</a></li>
             <li><a href="#">DC Meeting</a></li>
